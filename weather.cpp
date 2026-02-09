7f4cb38d3b246aa7e3897a89cf909c37388dc2b0
serenity-cell <justin1726hernandez2@gmail.com>
2026-02-09 17:46:36 -0500

    added humidity function

--- weather.cpp
+++ weather.cpp
@@ -1,5 +1,6 @@
 #include "weather.hpp"
-double weather_reading::temperature = 0;
+double weather_reading::temperature = 23.3;
+double weather_reading::humidity = 43.2;
 
 double weather_reading::get_random_value (const std::string& time) {
     double value = 0;
@@ -15,12 +16,12 @@ double weather_reading::get_random_value (const std::string& time) {
         maximum = 0.4;
     }
     else if (time == "temperature") {
-        minimum = -1.5;
-        maximum = 1.5;
+        minimum = 0.1;
+        maximum = 2.5;
     }
     else if (time == "humidity") {
-        minimum = -3;
-        maximum = 3;
+        minimum = 0.1;
+        maximum = 1.3;
     }
     else if (time == "pressure") {
         minimum = 0.2;
@@ -37,21 +38,29 @@ double weather_reading::get_random_value (const std::string& time) {
 
 }
 
-
-double weather_reading::get_temp(const std::string& time) {
-
+double weather_reading::get_temp(const int& time) {
     //daytime temperature changes
-    if (time == "day") {
+    if (time < 9) {
         temperature= temperature + get_random_value("day");
     }
     //nighttime temperature changes
-    else if  (time == "night") {
+    else if  (time >= 10) {
         temperature= temperature - get_random_value("night");
     }
     return temperature;
 
 };
 
-//void weather_reading::get_humidity() {
+double weather_reading::get_humidity(double old_data, double new_data) {
+    double delta = new_data - old_data;
+    if (delta < 0) {
+      humidity = humidity + get_random_value("humidity") + 0.3;
+    }
+    else if (delta > 0) {
+       humidity = humidity - get_random_value("humidity") - 0.3;
+    }
+    humidity = round(humidity);
 
-//}
+    return humidity;
+
+}
