24c225e7e71ddf51e605222482d504f13384b5e1
serenity-cell <justin1726hernandez2@gmail.com>
2026-02-09 17:48:04 -0500

    setting function types and parameters for get_temp and get_humidity

--- weather.hpp
+++ weather.hpp
@@ -18,8 +18,8 @@ public:
     static double pressure;
 
     static double get_random_value(const std::string& time);
-    static double get_temp(const std::string &time);
-    void get_humidity();
+    static double get_temp(const int &time);
+    static double get_humidity(double old_data, double new_data);
     void get_pressure();
 };
 
