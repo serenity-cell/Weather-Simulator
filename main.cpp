59aa33418fcf10e69e25457b564594d56df64019
serenity-cell <justin1726hernandez2@gmail.com>
2026-02-09 21:57:33 -0500

    added pressure to the main loop

--- main.cpp
+++ main.cpp
@@ -8,10 +8,12 @@ int main(){
         double old_temp = weather_reading::temperature;
         double new_temp = weather_reading::get_temp(time);
         if (time < 9) {
-            std::cout << "day: " << "\n temp: " << new_temp << "\n humidity: " << weather_reading::get_humidity(old_temp, new_temp) << "\n---------------- \n ";
+            std::cout << "day: " << "\n temp: " << new_temp << "\n humidity: "
+            << weather_reading::get_humidity(old_temp, new_temp) << "\n pressure: " << weather_reading::get_pressure() << "\n---------------- \n ";
         }
         else {
-            std::cout << "night: " << "\n temp: "<< new_temp << "\n humidity: "  << weather_reading::get_humidity(old_temp, new_temp) << "\n---------------- \n ";
+            std::cout << "night: " << "\n temp: "<< new_temp << "\n humidity: "
+            << weather_reading::get_humidity(old_temp, new_temp) << "\n pressure: " << weather_reading::get_pressure()  << "\n---------------- \n ";
 
         }
     }
