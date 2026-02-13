d7f6b21fa2a6c177abcdc45a3b80ad12c56eb5a7
serenity-cell <justin1726hernandez2@gmail.com>
2026-02-13 06:52:34 -0500

    setting foundation for weather log

---
+++ weather_features.hpp
@@ -0,0 +1,48 @@
+#pragma once
+#include <vector>
+#include <fstream>
+#include <string>
+#include <ctime>
+#include <cmath>
+
+
+
+#ifndef WEATHER_SIMULATOR_WEATHER_H
+#define WEATHER_SIMULATOR_WEATHER_H
+
+
+
+class weather_reading {
+    public:
+    static double temperature ;
+    static double humidity;
+    static double pressure;
+
+    static double get_random_value(const std::string& type);
+    static double get_temp(int time);
+    static double get_humidity(double old_data, double new_data);
+    static double get_pressure();
+};
+
+class weather_station {
+    public:
+    static void update(int time);
+    void get_reading();
+
+};
+
+class weather_log {
+    std::vector<double> weather_vector;
+    std::ifstream load_file;
+    std::ofstream save_file;
+
+public:
+    weather_log();
+    ~weather_log();
+    void record(double temp, double humidity, double pressure);
+    static void calculate_daily_average();
+    static void export_csv();
+};
+
+
+#endif //WEATHER_SIMULATOR_WEATHER_H
