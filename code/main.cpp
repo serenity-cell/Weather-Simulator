5aafb5369d720e70341c7ada59d8c674701d7b1f
justin <justin1726hernandez2@gmail.com>
2026-02-19 23:21:40 -0500

    changed the main loop to include the record function

--- code/main.cpp
+++ code/main.cpp
@@ -4,9 +4,17 @@
 int main(){
 
     weather_reading::temperature += weather_reading::get_random_value("temperature");
+    // Create weather_log object to record data
+    weather_log logger;
+    
     //MAIN CODE\LOOP
-    for (int time = 0; time < 25 ; time++) {
-        std::cout << time << "\n";
-        weather_station::update(time);
+    for (int time = 1; time < 24 ; time++) {
+        std::cout << time + 1 << "\n";
+        double temp = weather_reading::get_temp(time + 1);
+        double humidity = weather_reading::get_humidity(weather_reading::temperature, temp);
+        double pressure = weather_reading::get_pressure();
+        weather_station::update(time + 1);
+        // Record the weather data to file
+        logger.record(temp, humidity, pressure);
     }
 }
